3df302890a38ae2af8f8ceba4162b2d39fc33e33
Patrick Putman <putman.patrick@gmail.com>
2011-06-20 17:27:10 -0500

    Basic Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Limestoneema::Application.config.secret_token = '16eadfe2b2fac8720e0926bef9d4ba4979ffc8f47e1a90f3f71168c8b8699a9b05822ce05dc4a6e8f10e164288f9e661b16187b1a68ffd59f2cd503d28a2ae11'
